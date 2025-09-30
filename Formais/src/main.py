e3bdf27572e41a65f289b710b482c77cc022fc9a
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2025-09-30 16:23:04 -0300

    fix: output interation

--- Formais/src/main.py
+++ Formais/src/main.py
@@ -23,6 +23,7 @@ def main():
                 novo_automato = importa_automato()
                 if novo_automato:
                     automato = novo_automato
+                    processa_cadeia(automato)
             
             case "2":
                 if automato is None:
@@ -38,7 +39,8 @@ def importa_automato() -> Optional[AutomatoDePilha]:
 
     while True:
         print("==== Importação do autômato ==== ")
-        caminho_arquivo = input("Caminho do arquivo .json (ou 0 para voltar): \n")
+        caminho_arquivo = input("Caminho do arquivo .json (ou 0 para voltar): ")
+        print()
 
         if (caminho_arquivo == "0"):
             return None
@@ -46,25 +48,31 @@ def importa_automato() -> Optional[AutomatoDePilha]:
         try:
             leitor = LeitorJson()
             data = leitor.carrega_automato(caminho_arquivo)
-            print("Autômato carregado com sucesso!\n")
+            print("Autômato carregado com sucesso!")
 
             automato = AutomatoDePilha(data)
             automato.validar_estrutura()
-            print("Autômato validado com sucesso!")
+            print("Autômato validado com sucesso!\n")
             return automato
         
         except Exception as e:
             print(f"Erro ao importar / validar autômato: {e}\n")
 
 def processa_cadeia(automato: AutomatoDePilha) -> None:
-    print("==== Processamento de cadeias ====")
-    cadeia = input("Insira a cadeia que deseja processar: ")
 
-    try:
-        resultado = automato.simular_nao_deterministico(cadeia)
-    
-    except Exception as e:
-        print(f"Erro ao processar cadeia: {e}\n")
+    while True:
+        print("==== Processamento de cadeias ====")
+        cadeia = input("Insira a cadeia que deseja processar (ou 0 para voltar): ")
+        print()
+
+        if cadeia == "0":
+            return None
+
+        try:
+            resultado = automato.simular_nao_deterministico(cadeia)
+        
+        except Exception as e:
+            print(f"Erro ao processar cadeia: {e}\n")
 
 if __name__ == "__main__":
     main()
